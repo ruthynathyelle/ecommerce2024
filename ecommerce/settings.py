14dd02ff0453e073eb17e25f71bfe6bdc24518ac
Marcos Faino <marcos.faino@ifro.edu.br>
2024-08-07 22:13:36 -0400

    finalizando pedido

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'catproduto',
-    'carrinho', 
+    'carrinho',
+    'pedidos',
 ]
 
 MIDDLEWARE = [
@@ -65,6 +66,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'carrinho.context_processors.carrinho',
             ],
         },
     },
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('catproduto.urls')),
     path('carrinho/', include('carrinho.urls')),
+    path('pedidos/', include('pedidos.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
